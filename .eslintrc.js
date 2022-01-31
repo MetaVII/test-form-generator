32b1f0fb77a7c55579d2990a1ed99f70ef935ee8
Alex <alex.kryzhny@mail.ru>
2022-01-31 05:46:18 +0300

    feat: infrastructure

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["react-app", "airbnb", "prettier"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/"],
+      },
+    },
+  },
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/jsx-filename-extension": [
+      "error",
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+  },
+};
